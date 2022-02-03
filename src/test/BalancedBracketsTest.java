dc31807d67ead078199634ba8f70a4f875fd6153
System Administrator <root@Eswaramoorthis-MacBook-Air.local>
2022-02-03 08:58:18 -0500

    junit lc101 completed.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,10 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class BalancedBracketsTest {
 
@@ -12,5 +14,65 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+   @Test
+   public void frontBracketsReturnsTrue() {
+       assertTrue(BalancedBrackets.hasBalancedBrackets("test"));
+    }
+
+
+    @Test
+    public void singleBracketsAroundStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[test]"));
+    }
+
+    @Test
+    public void leftBracketOnlyReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[test"));
+    }
+
+    @Test
+    public void rightBracketOnlyReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("test]"));
+    }
+
+    @Test
+    public void multipleBracketsAroundStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[test]]]"));
+    }
+
+    @Test
+    public void doubleBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]test["));
+    }
+
+    @Test
+    public void middleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("te[]st"));
+    }
+
+    @Test
+    public void bothBracketsAtBeginningReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]test"));
+    }
+
+    @Test
+    public void bothBracketsAtEndReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test[]"));
+    }
+
+    @Test
+    public void singleBracketNoTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void multipleBracketPairsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Test] test [test test.]"));
+    }
 
 }
